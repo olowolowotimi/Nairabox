737bf1aa38d87e06dc9faaf2bd2c0a3543433410
olowolowotimi <62831854+olowolowotimi@users.noreply.github.com>
2021-01-19 13:03:10 +0100

    latest commit

--- pages/_app.js
+++ pages/_app.js
@@ -12,6 +12,8 @@ function MyApp({ Component, pageProps }) {
             name="viewport"
             content="minimum-scale=1, initial-scale=1, width=device-width"
           />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+<link href="https://fonts.googleapis.com/css2?family=Amaranth:ital@1&display=swap" rel="stylesheet" /> 
           
         </Head>
         <Component {...pageProps} />
